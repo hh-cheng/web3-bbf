1bb83df41ec5789aab873412d62a61885ce8ae73
kizuner-bonely <1003306162@qq.com>
2025-10-15 23:35:32 +0800

    feat: implement SSR

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Render } from '@nestjs/common'
+
 import { AppService } from './app.service'
 
 @Controller()
@@ -6,7 +7,16 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @Render('index')
+  root() {
+    const data = this.appService.getData()
+    return { data }
+  }
+
+  @Get('about')
+  @Render('about')
+  about() {
+    const data = this.appService.getAboutData()
+    return { data }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!'
+  getData() {
+    return 'Data from server'
+  }
+
+  getAboutData() {
+    return 'About data from server'
   }
 }
